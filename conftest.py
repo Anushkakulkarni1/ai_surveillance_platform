3d7159d606f7d4178f362b0d7863c16a19ce18a0
Anushka Kulkarni <223412914+Anushkakulkarni1@users.noreply.github.com>
2026-08-14 17:52:36 +0530

    style: format conftest.py with black

--- conftest.py
+++ conftest.py
@@ -1,10 +1,3 @@
-"""
-conftest.py
-
-Ensures `detection/` and `ml/` are importable from `tests/`, regardless
-of the current working directory pytest is invoked from.
-"""
-
 import os
 import sys
 
@@ -15,3 +8,4 @@ _ML_DIR = os.path.join(_REPO_ROOT, "ml")
 for _path in (_REPO_ROOT, _DETECTION_DIR, _ML_DIR):
     if _path not in sys.path:
         sys.path.insert(0, _path)
+
